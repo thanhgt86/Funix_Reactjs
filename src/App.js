4a0ba65a73fb980b9b55915806ef33d2eadc2dad
Minh Thanh <thanhnmFx16802@funix.edu.vn>
2022-07-28 22:37:12 +0700

    revise to make Lab02_2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Lab02.1</h1>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Lab02_2</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
