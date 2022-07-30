87043ff5746b2defceefed755a1fd599dd2133c8
Minh Thanh <thanhnmFx16802@funix.edu.vn>
2022-07-30 17:25:18 +0700

    revise to make Lab03.2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Lab03_1</NavbarBrand>
+            <NavbarBrand href="/">Lab03_2</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
