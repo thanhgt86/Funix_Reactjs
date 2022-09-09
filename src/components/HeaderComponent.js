d34138d378713ed78b1ecb0c279937cf30d77175
Minh Thanh <thanhgt86@gmail.com>
2022-09-09 22:09:38 +0700

    cài đặt icon

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -44,48 +44,26 @@ class Header extends Component {
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
-                  <NavLink className="nav-link" to="/home">
-                    <span className="fa fa-home fa-lg"></span> Home
+                  <NavLink className="nav-link" to="/stafflist">
+                    <span className="fa fa-user-circle fa-lg"></span> Nhân Viên
                   </NavLink>
                 </NavItem>
 
                 <NavItem>
                   <NavLink className="nav-link" to="/aboutus">
-                    <span className="fa fa-info fa-lg"></span> About Us
+                    <span className="fa fa-th fa-lg"></span> Phòng ban
                   </NavLink>
                 </NavItem>
 
                 <NavItem>
                   <NavLink className="nav-link" to="/menu">
-                    <span className="fa fa-list fa-lg"></span> Menu
-                  </NavLink>
-                </NavItem>
-
-                <NavItem>
-                  <NavLink className="nav-link" to="/contactus">
-                    <span className="fa fa-address-card fa-lg"></span> Contact
-                    Us
+                    <span className="fa fa-list fa-lg"></span> Bảng Lương
                   </NavLink>
                 </NavItem>
               </Nav>
             </Collapse>
           </div>
         </Navbar>
-
-        <Jumbotron>
-          <div className="container">
-            <div className="row row-header">
-              <div className="col-12 col-sm-6">
-                <h1>Ristorante con Fusion</h1>
-                <p>
-                  We take inspiration from the World's best cuisines, and create
-                  a unique fusion experience. Our lipsmacking creations will
-                  tickle your culinary senses!
-                </p>
-              </div>
-            </div>
-          </div>
-        </Jumbotron>
       </React.Fragment>
     );
   }
