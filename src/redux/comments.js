c966b73a259c509c265be83254a998b8060353fe
Minh Thanh <thanhgt86@gmail.com>
2022-11-09 20:04:34 +0700

    Make Exercise 4.3

---
+++ src/redux/comments.js
@@ -0,0 +1,15 @@
+import * as ActionTypes from "./ActionTypes";
+import { COMMENTS } from "../shared/comments";
+
+export const Comments = (state = COMMENTS, action) => {
+  console.log(action);
+  switch (action.type) {
+    case ActionTypes.ADD_COMMENT:
+      var comment = action.payload;
+      comment.id = state.length;
+      comment.date = new Date().toISOString();
+      return state.concat(comment);
+    default:
+      return state;
+  }
+};
