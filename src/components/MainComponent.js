7061d6523fba4a2b59f25caa1308a68ce7ccf41d
Minh Thanh <thanhgt86@gmail.com>
2022-11-09 20:06:27 +0700

    Make Exercise 4.4

---
+++ src/components/MainComponent.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+
+import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
+import DishDetail from "./DishdetailComponent";
+import About from "./AboutComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  postComment,
+  fetchDishes,
+  fetchPromos,
+  fetchComments,
+} from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
+
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
+
+  resetFeedbackForm: () => {
+    dispatch(actions.reset("feedback"));
+  },
+
+  fetchComments: () => {
+    dispatch(fetchComments());
+  },
+
+  fetchPromos: () => {
+    dispatch(fetchPromos());
+  },
+});
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
+  }
+
+  render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
+          promotion={
+            this.props.promotions.promotions.filter(
+              (promo) => promo.featured
+            )[0]
+          }
+          promosLoading={this.props.promotions.isLoading}
+          promosErrMess={this.props.promotions.errMess}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
+    };
+
+    const DishWithId = ({ match }) => {
+      console.log(match);
+
+      return (
+        <DishDetail
+          dish={
+            this.props.dishes.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+          comments2={this.props.comments.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+          commentsErrMess={this.props.comments.errMess}
+          postComment={this.props.postComment}
+        />
+      );
+    };
+
+    return (
+      <div>
+        <Header />
+        <TransitionGroup>
+          <CSSTransition
+            key={this.props.location.key}
+            classNames="page"
+            timeout={300}
+          >
+            <Switch>
+              <Route path="/home" component={HomePage} />
+              <Route
+                exact
+                path="/menu"
+                component={() => <Menu dishes={this.props.dishes} />}
+              />
+              <Route exact path="/menu/:dishId" component={DishWithId} />
+              <Route
+                exact
+                path="/contactus"
+                component={() => (
+                  <Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+                )}
+              />
+              <Route
+                exact
+                path="/aboutus"
+                component={() => <About leaders={this.props.leaders} />}
+              />
+              <Redirect to="/home" />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
